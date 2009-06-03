2de0bb1f60207e4836bd4d120d869b0263183963
Snipi <Snipi@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-06-03 15:37:04 +0000

    [+] remove wordpress settings from user account on Snipi.com
    git-svn-id: https://plugins.svn.wordpress.org/snipi-for-wordpress-media-library-and-nextgen-gallery/trunk@122656 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- core.php
+++ core.php
@@ -220,6 +220,11 @@ class snipiLoader{
 }
 
 /**
+ * Username on Snipi.com
+ */
+$snipi_username='';
+
+/**
  * Generate api for current user
  *
  * @return string Hash string
@@ -254,6 +259,7 @@ function wp_snipi_get_url(){
  * @return boolean
  */
 function wp_snipi_is_active($api){
+    global $snipi_username;
     // create a new cURL resource
     $ch=curl_init();
     // set URL and other appropriate options
@@ -273,6 +279,7 @@ function wp_snipi_is_active($api){
 			$obj = $json->decode($buffer);
         }
         if ($obj->success=='true'&&$obj->wp_url==wp_snipi_get_url()){
+            $snipi_username=$obj->username;
             return true;
         }
     }
@@ -289,8 +296,43 @@ function wp_snipi_is_active($api){
  * @return boolean
  */
 function wp_snipi_update_user($un,$pwd,$api,$url){
+    global $snipi_username;
     // set URL and other appropriate options
     $servis_url=SNIPI_AJAX_URL.'?service=updatewpapi&username='.urlencode($un).'&password='.urlencode($pwd).'&url='.urlencode($url).'&api='.urlencode($api);
+    // create a new cURL resource
+    $ch=curl_init();
+    curl_setopt($ch,CURLOPT_URL,$servis_url);
+    curl_setopt($ch,CURLOPT_CONNECTTIMEOUT,2);
+    curl_setopt($ch,CURLOPT_RETURNTRANSFER,1);
+    $buffer=curl_exec($ch);
+    // close cURL resource, and free up system resources
+    curl_close($ch);
+    if (!empty($buffer)){
+        if (function_exists('json_decode'))
+        	$obj=json_decode($buffer);
+        else{
+        	require_once (dirname(__FILE__).'/lib/json.php');
+        	$json = new Services_JSON();
+			$obj = $json->decode($buffer);
+        }
+        if ($obj->success=='true'){
+            $snipi_username=$obj->username;
+            return true;
+        }
+    }
+    return false;
+}
+
+/**
+ * Remove any reference to this plugin activation on Snipi.com
+ *
+ * @param string $api
+ * @param string $url
+ * @return unknown
+ */
+function wp_snipi_remove_user($api,$url){
+    // set URL and other appropriate options
+    $servis_url=SNIPI_AJAX_URL.'?service=removeapi&url='.urlencode($url).'&api='.urlencode($api);
     // create a new cURL resource
     $ch=curl_init();
     curl_setopt($ch,CURLOPT_URL,$servis_url);
@@ -313,6 +355,7 @@ function wp_snipi_update_user($un,$pwd,$api,$url){
     }
     return false;
 }
+
 
 /**
  * Get information about user who has particular api
