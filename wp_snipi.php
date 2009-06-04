f9c8f68f2066a1a45ef2e666fdee89428e1a0563
Snipi <Snipi@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-06-04 22:17:13 +0000

    git-svn-id: https://plugins.svn.wordpress.org/snipi-for-wordpress-media-library-and-nextgen-gallery/trunk@122973 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- wp_snipi.php
+++ wp_snipi.php
@@ -4,14 +4,14 @@ Plugin Name: Snipi for Wordpress
 Plugin URI: http://snipi.com/tour/view/social/
 Description: Drag and drop images into the Snipi Toolbar and they automatically appear in your WordPress Media Library or NextGen Gallery. <a href="admin.php?page=snipi-for-wordpress-media-library-and-nextgen-gallery">Configure</a>.  Get the toolbar (<a href="http://www.snipi.com">www.snipi.com</a>).
 Author: Snipi, Inc <info@snipi.com>
-Version: 1.1.0
+Version: 1.1.1
 Author URI: http://snipi.com/tour/view/social/
 */
 /**
  * Plugin to upload images from Snipi.com to WordPress gallery
  * @package wp_snipi
  * @author Denis Uraganov <snipi@uraganov.net>
- * @version 1.1.0
+ * @version 1.1.1
  */
 //only allow to call this page directly when image url and api specified
 if (preg_match('#'.basename(__FILE__).'#',$_SERVER['PHP_SELF'])&&(!isset($_GET['api'])||!isset($_GET['img_url']))){
